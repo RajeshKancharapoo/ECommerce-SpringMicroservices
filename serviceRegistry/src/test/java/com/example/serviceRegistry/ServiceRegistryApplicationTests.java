37c3296dce3b2f1dd3b5d2c233cb215b18ad885b
RajeshKancharapoo <160219573+RajeshKancharapoo@users.noreply.github.com>
2024-02-25 16:38:43 +0530

    Add files via upload

---
+++ serviceRegistry/src/test/java/com/example/serviceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
